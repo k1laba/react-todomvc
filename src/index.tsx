f69514edd23fa91c83f9635f825d4dd0a3167176
k1laba <me@beqa.ge>
2021-08-22 12:58:07 +0400

    Update node-sass package

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,8 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import rootReducer from './store/reducer';
 
-const composeEnhancers = process.env.NODE_ENV === 'development' ? (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__: null || compose;
+const composeEnhancers = process.env.NODE_ENV === 'development' 
+? (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__: null || compose;
 const store = createStore(rootReducer, composeEnhancers());
 
 
